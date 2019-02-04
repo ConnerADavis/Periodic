39e23baa5be7fee9bd4c09ba34c44cb4c9d6b132
Conner Davis <conner.davis.17@cnu.edu>
2019-02-04 13:10:58 -0500

    Created Interaction Window class

---
+++ Periodic/src/InteractionWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+public class InteractionWindow extends JFrame {
+    
+    
+    public static void main(String[] args) {
+        JFrame f = new InteractionWindow();
+        f.setVisible(true);
+    }
+}
