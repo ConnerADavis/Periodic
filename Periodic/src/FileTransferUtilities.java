f16a43f5444bf854c7341e0ada32c9e4f0db899b
Conner Davis <conner.davis.17@cnu.edu>
2019-03-25 11:32:49 -0400

    File Upload ready

--- Periodic/src/FileTransferUtilities.java
+++ Periodic/src/FileTransferUtilities.java
@@ -2,10 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 
-import org.apache.commons.net.PrintCommandListener;
-import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
 /*import it.sauronsoftware.ftp4j.FTPAbortedException;
@@ -20,58 +17,42 @@ public class FileTransferUtilities {
     {
         
         FTPClient ftp = new FTPClient();
-        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+        
         try {
             ftp.connect("ec2-18-220-0-36.us-east-2.compute.amazonaws.com");
-            ftp.login(username, password);
-            ftp.setFileType(FTP.BINARY_FILE_TYPE);
+            check(ftp, "login", ftp.login(username, password));
+            System.out.println("Successfully Connected to server");
             
             File container = new File("Files");
             File[] uploadFiles = container.listFiles();
+            check(ftp, "cd", ftp.changeWorkingDirectory("username"));
+            
             for (File uploadfile : uploadFiles)
             {
                 if(uploadfile.isFile())
                 {
+                    InputStream input = new FileInputStream(uploadfile.getAbsolutePath());
                     System.out.println(uploadfile.getName());
-                    InputStream input = new FileInputStream(uploadfile);
-                    ftp.storeFile(uploadfile.getName(), input);
+                    System.out.println(ftp.printWorkingDirectory());
+                    System.out.println(ftp.printWorkingDirectory());
+                    check(ftp, "store", ftp.storeFile(uploadfile.getName(), input));
                 }
             }
-            ftp.logout();
-            ftp.disconnect();
-
             return true;
         }
         catch (IOException e) {
-            // TODO Auto-generated catch block
+            
             e.printStackTrace();
             return false;
         }
-        /*FTPClient client = new FTPClient();
-        
-        try {
-            client.connect("ec2-18-220-0-36.us-east-2.compute.amazonaws.com");
-            client.login(username, password);
-            client.createDirectory("Files");
-            
-            File container = new File("Files");
-            File[] uploadFiles = container.listFiles();
-            for (File uploadfile : uploadFiles)
-            {
-                if(uploadfile.isFile())
-                {
-                    System.out.println(uploadfile.getName());
-                    client.upload(uploadfile);
-                }
-            }
-            /*File[] listofFiles = container.listFiles();
-            for(File f: listofFiles)
-            {
-                if (f.getName().contains(".jar"))
-                {
-                    continue;
-                }
-                client.upload(f);
-            }*/
+    }
+    
+    
+    private static void check(FTPClient ftp, String cmd, boolean succeeded) throws IOException 
+    {
+        if (!succeeded) 
+        {
+          throw new IOException("FTP error: " + ftp.getReplyString());
+        }
     }
 }
